07854d2b8bd1dd4dedec923bc13f0fcba20e362d
ivyaccount <ivy7m@yahoo.com.tw>
2019-06-22 20:49:13 +0800

    revise a bug at initial

--- Bomb/Bomb.cpp
+++ Bomb/Bomb.cpp
@@ -3,6 +3,8 @@
 void Bomb::initial(int x, int y, float countdown, Tilemap &map){
     this->pos_x = x;
     this->pos_y = y;
+    this->tile_i = (pos_y - MAP_Y0) / TILESIZE;
+    this->tile_j = (pos_x - MAP_X0) / TILESIZE;
 
     this->anim.Initialize(game_bomb, 4, ALLEGRO_MSECS_TO_SECS(150), true);
 
@@ -10,7 +12,7 @@ void Bomb::initial(int x, int y, float countdown, Tilemap &map){
     this->time_until_exploded = countdown;
     this->timer = al_get_time();
 
-    map.SetBombPos(x,y,1);
+    map.SetBombPos(tile_i,tile_j,1);
 }
 void Bomb::update(Tilemap &map){
     map.SetBombPos(tile_i, tile_j, 0);
